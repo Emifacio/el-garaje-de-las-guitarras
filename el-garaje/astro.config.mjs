683a4496eb72d7886cc95f66a900c432870518c6
Gabriel Emiliano Facio <127351922+Emifacio@users.noreply.github.com>
2026-03-08 09:20:33 -0300

    Add SEO optimizations

--- el-garaje/astro.config.mjs
+++ el-garaje/astro.config.mjs
@@ -2,14 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.elgarajedelasguitarras.com',
   vite: {
     plugins: [tailwindcss()]
   },
+  integrations: [sitemap()],
   output: 'static',
   adapter: vercel(),
   redirects: {
